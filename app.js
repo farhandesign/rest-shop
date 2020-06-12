e9ed3b7705919f096f1c82fe2f8bc8aeff3b6ae4
Farhan Hasan <farhan.design@outlook.com>
2020-06-12 18:18:22 +0500

    Added basic products and orders routes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works'
-  });
-});
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
