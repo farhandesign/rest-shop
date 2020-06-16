20c1a846edd4a67c6e6da621bb0ce7f8b06ec442
Farhan Hasan <farhan.design@outlook.com>
2020-06-16 22:29:29 +0500

    JWT Route Protection

--- api/routes/user.js
+++ api/routes/user.js
@@ -62,9 +62,10 @@ router.post('/login', (req, res, next) => {
           });
         }
         if (result) {
-          const token = jwt.sign({
-            email: user[0].email,
-            userId: user[0]._id
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
             },
             process.env.JWT_KEY,
             {
